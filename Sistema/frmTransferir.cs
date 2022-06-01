489f1def1677fbf2ddf693755a202f3aa5be3599
g00gle2401 <cruzhitha146@gmail.com>
2022-06-01 13:43:16 -0500

    Codigo Transferir

--- Sistema/frmTransferir.cs
+++ Sistema/frmTransferir.cs
@@ -34,7 +34,69 @@ namespace Sistema
         }
         public void transferir()
         {
-            
+            cuenta = txtCuenta.Text;
+            cantidad = int.Parse(txtMontoT.Text);
+            id = Session.id_usuario;
+            if (string.IsNullOrEmpty(cantidad.ToString()) || string.IsNullOrEmpty(cuenta))
+            {
+                MessageBox.Show("Debe ingresar la cuenta receptora y el monto de envio", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                if (modelo.existeCuenta(cuenta))
+                {
+                    MySqlConnection conexion = Conexion.getConexion();
+                    conexion.Open();
+
+                    String query = "UPDATE usuarios SET saldo = (saldo + @cantidad) WHERE cuenta LIKE @cuenta";
+                    MySqlCommand comando = new MySqlCommand(query, conexion);
+                    comando.Parameters.AddWithValue("@cantidad", cantidad);
+                    comando.Parameters.AddWithValue("@cuenta", cuenta);
+                    int resultado = comando.ExecuteNonQuery();
+
+                    String query1 = "UPDATE usuarios SET saldo = (saldo - @cantidad) WHERE id_usuario LIKE @id";
+                    MySqlCommand comando1 = new MySqlCommand(query1, conexion);
+                    comando1.Parameters.AddWithValue("@cantidad", cantidad);
+                    comando1.Parameters.AddWithValue("@id", id);
+                    int resultado1 = comando1.ExecuteNonQuery();
+
+                    try
+                    {
+                        if ((resultado == 1) && (resultado1 == 1))
+                        {
+                            MessageBox.Show("Transferencia exitosa");
+
+                            i = Session.i;
+                            string Date = DateTime.Now.ToString("G");
+                            string op = "Transferencia";
+                            string sql = "INSERT INTO historial (id_usuario, id_cajero, fecha, cantidadH, movimiento) VALUES(@id_usuario, @id_cajero, @fecha, @cantidad, @movimiento)";
+                            MySqlCommand comand = new MySqlCommand(sql, conexion);
+                            comand.Parameters.AddWithValue("@id_usuario", id);
+                            comand.Parameters.AddWithValue("@id_cajero", i);
+                            comand.Parameters.AddWithValue("@fecha", Date);
+                            comand.Parameters.AddWithValue("@cantidad", cantidad);
+                            comand.Parameters.AddWithValue("@movimiento", op);
+
+                            int res = comand.ExecuteNonQuery();
+                        }
+                        else
+                        {
+                            MessageBox.Show("ERROR, NO se pudo depositar");
+                        }
+                        txtMontoT.Text = "";
+
+                    }
+                    catch (MySqlException ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("El número de cuenta no existe", "Aviso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
         }
         public void date()///
         {
